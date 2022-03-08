ac95f661f4649b0fd02ac1ad910630f2e6085bae
nyankonabe <hasishun67@gmail.com>
2022-03-08 11:59:06 +0900

    init

---
+++ src/App.svelte
@@ -0,0 +1,14 @@
+<script>
+	let firstName = '';
+	let lastName = '';
+	$: fullName = `${firstName} ${lastName}`;
+  </script>
+  
+  <main>
+	<input bind:value={firstName}>
+	<input bind:value={lastName}>
+	<div>Hello, {fullName}</div>
+  </main>
+  
+  <style>
+  </style>
